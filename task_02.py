e6ffadd33482f7767c87e2a6acd6784b608be676
Ambreen Ahmed <ambahm.aa@gmail.com>
2015-02-17 04:53:41 +0000

    Wk# Synth T3: Replacing first word Using slice, index(), len() and cancatenation(+)

--- task_02.py
+++ task_02.py
@@ -4,3 +4,11 @@
 
 
 import inquisition
+
+# from inquisition import SPANISH
+FILE = inquisition.SPANISH
+
+WO_REP = 'Spanish'  # first instance of Spanish to be removed at index 0
+WO_LEN = len(WO_REP)  # lenght of word to be removed
+FLEMISH = FILE[0:FILE.index(WO_REP)]+'Flemish'+FILE[FILE.index(WO_REP)+WO_LEN:]
+print "\n", FLEMISH
