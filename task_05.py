4ea66c476c95d02eb41b3805b949427ef29207ab
Ambreen Ahmed <ambahm.aa@gmail.com>
2015-02-17 06:41:42 +0000

    Wk 3 SynthT5 Corrected error Replaced != from is not

--- task_05.py
+++ task_05.py
@@ -25,6 +25,7 @@ def get_member_count(my_sequence):
     """
     try:
         length = len(my_sequence)
+
     except TypeError:
         length = False
 
@@ -61,8 +62,9 @@ def is_empty(my_sequence):
     """
     count = get_member_count(my_sequence)
 
-    if count != False:
+    if count is not False:  # Correcting error replace !=
         return count == 0
+
     else:
         raise TypeError('Object has no len()')
 
